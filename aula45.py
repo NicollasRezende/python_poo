5e49b00d98ca50de83eb816b9c0161ee34c5ba7f
NICOLLAS PEREIRA REZENDE <nicollaspereira025@gmail.com>
2023-09-20 16:01:46 -0300

    aula_python

--- aula45.py
+++ aula45.py
@@ -31,7 +31,9 @@ Banco será responsável autenticar o cliente e as contas da seguinte maneira:
 Só será possível sacar se passar na autenticação do banco (descrita acima)
 Banco autentica por um método.
 """
+
 from abc import ABC, abstractmethod
+
 
 class Cria_Pessoa(ABC):
 
@@ -103,6 +105,194 @@ class Banco(ABC):
     @senha.setter
     def definir_senha(self, senha_def):
         self.senha = senha_def
+
+
+class ContaCorrente(Banco):
+    def __init__(self, tipoconta, nomebanco, saldo, senha) -> None:
+        super().__init__(tipoconta, nomebanco, saldo, senha)
+        self.definir_saldo = 0
+        self.definir_nome_banco ='Nenhum'
+        self.definir_senha = 12
+        self.definir_tipo_conta = 'Nenhum'
+        self.sacar = True
+        self.depositar = True
+        self.verificar_saldo = True
+        self.fazer_tranferencia = True
+
+
+
+class ContaPoupanca(Banco):
+    def __init__(self, tipoconta, nomebanco, saldo, senha) -> None:
+        super().__init__(tipoconta, nomebanco, saldo, senha)
+        self.definir_saldo = 0
+        self.definir_nome_banco ='Nenhum'
+        self.definir_senha = 12
+        self.definir_tipo_conta = 'Nenhum'
+        self.sacar = True
+        self.depositar = True
+        self.verificar_saldo = True
+        self.fazer_tranferencia = False
+
+
+#---------------------CONF DE CRIACAO----------------------------
+import random
+CONTA_GERADA = None
+BANCO = None
+NOME = None
+IDADE = None
+TRABALHO = None
+SENHA = None
+TIPO_CONTA = None
+USER = None
+MENU = True
+def gerar_numero_conta():
+    NUMERO_CONTA = random.randint(1000000, 9999999)
+    return NUMERO_CONTA
+
+#---------------------CRIACAO DA CONTA----------------------------
+criar = input('Deseja criar uma conta?[S]im, [N]ao: ')
+
+if criar == 'S':
+
+    criar2 = input('Qual Banco? 1-Bradesco, 2-Santander, 3-Banco do Brasil: ')
+
+    if criar2 == 1:
+        BANCO = 'Bradesco'
+    if criar2 == 2:
+        BANCO = 'Santander'
+    if criar2 == 3:
+        BANCO = 'Banco_do_Brasil'
+
+    user_nome = input('Digite seu nome: ')
+    user_idade = input('Digite sua idade: ')
+    user_emprego = input('Voce trabalha de que? : ')
+    user_tipo_conta = input('Escolha um tipo de conta, Conrrente-1, Poupança-2: ')
+    user_senha_conta = input('crie uma senha de 4 digitos: ')
+
+    CONTA_GERADA = gerar_numero_conta()
+    NOME = user_nome
+    IDADE = user_idade
+    TRABALHO = user_emprego
+    SALDO = 0
+    SENHA = user_senha_conta
+    print(f'o Banco escolhido foi {BANCO}')
+    print(f'o numero da sua conta e {CONTA_GERADA}')
+    print(f'o seu nome e {NOME}')
+    print(f'voce tem {IDADE} anos')
+    print(f'voce trabalha como {TRABALHO}')
+    print(f'Sua senha escolhida foi: {SENHA}')
+
+
+    if user_tipo_conta == '1':
+        TIPO_CONTA = 'Corrente'
+        USER = ContaCorrente('Corrente', BANCO, SALDO, SENHA)
+
+    if user_tipo_conta == '2':
+        TIPO_CONTA = 'Poupança'
+        USER = ContaPoupanca('Poupança', BANCO, SALDO, SENHA)
+    
+    USER.definir_nome_banco = BANCO
+    USER.tipo_conta = TIPO_CONTA
+    USER.definir_nome_pessoa = NOME
+    USER.definir_idade_pessoa = IDADE
+    USER.definir_trabalho_pessoa = TRABALHO
+    USER.definir_saldo = SALDO
+
+else: 
+    print('sifode entao')
+    MENU = False
+
+
+#---------------------MENU BANCO----------------------------
+
+while MENU:
+    print()
+    print(f'BEM VINDO AO MENU DO {BANCO}')
+    print()
+    print('O QUE VOCE DESEJA REALIZAR:')
+    print('1- ACESSAR INFORMAÇOES DA CONTA')
+    print('2- ACESSAR MENU DE TRANSFERENCIA')
+    print('3- CONSULTAR O SALDO DA CONTA')
+    print('4- MENU DE DEPOSITO')
+    print('5- MENU DE SAQUE')
+    print('6- SAIR')
+    
+    usuario1 = input('Digite o numero da opçao: ')
+
+
+    if usuario1 == 1:
+        print()
+        print(f'SEU BANCO: {BANCO}')
+        print(f'NUMERO DA SUA CONTA: {CONTA_GERADA}')
+        print(f'SEU NOME: {NOME}')
+        print(f'SUA IDADE: {IDADE}')
+        print(f'SUA PROFIÇAO: {TRABALHO}')
+        print(f'SUA SENHA:{SENHA}')
+        continue
+    if usuario1 == 2 and USER.fazer_tranferencia == True:
+        print()
+        print('-MENU DE TRANFERENCIA-')
+        trasnferir1 = int(input('Quanto deseja tranferir:'))
+        if trasnferir1 < SALDO:
+            print('voce nao tem saldo suficiente para fazer isso.')
+        else:
+            print()
+            digitar_senha1 = int(input('DIGITE SUA SENHA DE 7 DIGITOS:'))
+            if digitar_senha1 == SENHA:
+                SALDO -= trasnferir1
+                USER.definir_saldo(SALDO)
+                print()
+                print(f'Sua transferencia no valor de {trasnferir1} foi concluida com sucesso!')
+                print(f'Seu saldo atual e de {SALDO}')
+                continue
+            else:
+                print('nao foi possivel realizar a transferencia')
+                continue
+    if usuario1 == 3:
+        print()
+        print(f'O SEU SALDO E DE:')
+        print(f'({SALDO})')
+        continue
+    if usuario1 == 4:
+        print()
+        print('-MENU DE DEPOSITO-')
+        print()
+        digitar_senha1 = int(input('DIGITE SUA SENHA DE 7 DIGITOS:'))
+        if digitar_senha1 == SENHA:
+            print()
+            deposito1 = int(input('QUANTO VOCE DESEJA DEPOSITAR:'))
+            print()
+            SALDO += deposito1
+            USER.definir_saldo(SALDO)
+            print(f'DEPOSITO NO VALOR DE {deposito1} REALIZADO COM SUCESSO!')
+            print(f'SEU SALDO ATUAL E DE {SALDO}')
+            continue
+        else:
+            print('nao foi possivel realizar o deposito')
+            continue
+    if usuario1 == 5:
+        print()
+        print('-MENU DE SAQUE-')
+        digitar_senha1 = int(input('DIGITE SUA SENHA DE 7 DIGITOS:'))
+        if digitar_senha1 == SENHA:
+            print()
+            saque1 = input('QUANTO VOCE DESEJA SACAR?: ')
+            if saque1 > SALDO:
+                print(f'SEU SAQUE NO VALOR DE {saque1} NAO PODE SER REALIZADO POIS O SALDO DA SUA CONTA E MENOR DO QUE O VALOR SOLICITADO')
+                print(f'SEU SALDO E DE {SALDO}')
+                continue
+            if saque1 < SALDO:
+                print()
+                SALDO -= saque1
+                USER.definir_saldo(SALDO)
+                print(f'O SEU SAQUE NO VALOR DE {saque1} FOI REALIZADO COM SUCESSO!')
+                print(f'O SEU SALDO ATUAL E DE {SALDO}')
+                continue
+        else:
+            print('nao foi possivel realizar o saque')
+            continue
+
+
 
 
 
